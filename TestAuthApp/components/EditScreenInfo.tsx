0a1fa198a471ab620c891f6a608605e801939d7c
aman7398 <amandonaditya298@gmail.com>
2026-01-14 23:38:03 +0530

    normal changes - Aman

--- TestAuthApp/components/EditScreenInfo.tsx
+++ TestAuthApp/components/EditScreenInfo.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { ExternalLink } from './ExternalLink';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
-
 import Colors from '@/constants/Colors';
 
 export default function EditScreenInfo({ path }: { path: string }) {
@@ -38,7 +38,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.helpLink}
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Tap here if your app doesn&apos;t automatically update after making changes
           </Text>
         </ExternalLink>
       </View>
